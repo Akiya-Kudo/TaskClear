9268151460641a884ac158d0124210aebce5de1b
Akiya-Kudo <k.akiya07@gmail.com>
2022-08-08 15:46:06 +0900

    make tables $ seeded

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('locked_flg')->default(0);
+            $table->integer('error_count')->unsigned()->default(0); // unsignedは255まで記録できるようになるがマイナス値を使えなくなる
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
