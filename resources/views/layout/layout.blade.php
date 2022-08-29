cc955a8c1319a62b31f9fa468391d11fb3184ecc
Akiya-Kudo <k.akiya07@gmail.com>
2022-08-29 18:31:53 +0900

    change the order number of styleshet & js script

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title') </title>
-    {{-- scripts  --}}
-    <script src="{{ asset('js/app.js') }}" defer></script>
     <!-- CSS only -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <!-- Custom styles for this template -->
     @yield('stylesheet')
+    {{-- scripts  --}}
+    <script src="{{ asset('js/app.js') }}" defer></script>
     <!-- Custom scripts for this template -->
     @yield('script')
 </head>
